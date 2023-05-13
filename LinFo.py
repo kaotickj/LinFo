aae1e3b85d188b62224caa5bb38508529e9879f4
Johnny Watts <warhamr1488@gmail.com>
2023-05-12 22:02:38 -0400

    Update LinFo.py

--- LinFo.py
+++ LinFo.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-import os
+# Script: LinFo v 0.1
+# Author: kaotickj
+# Website: https://github.com/kaotickj
+
 import subprocess
 
 ####################
@@ -16,6 +19,7 @@ LG = f"{C}[1;37m"  # LightGray
 DG = f"{C}[1;90m"  # DarkGray
 NC = f"{C}[0m"
 
+
 def run_command(command):
     result = subprocess.run(
         command,
@@ -28,51 +32,66 @@ def run_command(command):
     else:
         return ""
 
+
 def get_os_info():
     os_info = run_command("lsb_release -sd 2>/dev/null")
     if not os_info:
         os_info = run_command("sw_vers -productName 2>/dev/null")
     return os_info
 
+
 def get_kernel_version():
     return run_command("uname -r")
+
 
 def get_host_name():
     return run_command("hostname")
 
+
 def get_uptime():
     return run_command("uptime -p")
+
 
 def get_package_count():
     return run_command("dpkg-query -f '${binary:Package}\n' -W 2>/dev/null | wc -l")
 
+
 def get_desktop_environment():
     return run_command("echo $XDG_CURRENT_DESKTOP | tr '[:upper:]' '[:lower:]'")
+
 
 def get_resolution():
     return run_command("xdpyinfo | awk '/dimensions:/ {print $2}'")
 
+
 def get_terminal():
     return run_command("echo $TERM")
+
 
 def get_shell():
     return run_command("basename $SHELL")
 
+
 def get_cpu_info():
     return run_command("lscpu | awk -F':' '/Model name/ {print $2}' | sed -e 's/^[ \\t]*//' 2>/dev/null")
 
+
 def get_gpu_info():
     return run_command("lspci | grep -i 'vga\\|3d' | awk -F': ' '{print $2}' 2>/dev/null")
+
 
 def get_memory_info():
     mem_total = run_command("grep MemTotal /proc/meminfo | awk '{print $2/1024/1024 \"GB\"}' 2>/dev/null")
     return f"{mem_total} (approximate)"
 
+
 def get_disk_usage():
     return run_command("df -h --total 2>/dev/null | awk '/total/ {print $2 \" used, \" $4 \" free\"}'")
 
+
 def get_interfaces():
     return run_command("ip -o link show | awk -F': ' '{print $2}' | grep -v 'lo'")
+
 
 os = get_os_info()
 kernel = get_kernel_version()
@@ -91,7 +110,7 @@ interfaces = get_interfaces()
 
 print()
 print(f"\n\n{YELLOW}             ⚙️    LinFo   ⚙️")
-print(f"{LIGHT_MAGENTA}      🕵🔎 Powered by  KaotickJ 👽          \n") 
+print(f"{LIGHT_MAGENTA}      🕵🔎 Powered by  KaotickJ 👽          \n")
 print(f"{GREEN}    🐧 OS:{LIGHT_CYAN} {os}")
 print(f"{GREEN}    🏠 Host:{LIGHT_CYAN} {host}")
 print(f"{GREEN}    🌽 Kernel:{LIGHT_CYAN} {kernel}")
@@ -124,4 +143,5 @@ print("   ░ ▒░▓  ░░▓  ░ ▒░   ▒ ▒  ▒ ░   ░ ▒░�
 print("   ░ ░ ▒  ░ ▒ ░░ ░░   ░ ▒░ ░       ░ ▒ ▒░")
 print("     ░ ░    ▒ ░   ░   ░ ░  ░ ░   ░ ░ ░ ▒")
 print("       ░  ░ ░           ░            ░ ░")
-print(f"      🎈🎈🎈 {LIGHT_CYAN}y{YELLOW}O{BLUE}u{LIGHT_MAGENTA}'L{RED}l{YELLOW} f{LIGHT_CYAN}L{RED}o{LIGHT_CYAN}A{YELLOW}t{GREEN} {BLUE}t{LIGHT_MAGENTA}O{GREEN}o! 🎈🎈🎈        {NC}")
+print(
+    f"      🎈🎈🎈 {LIGHT_CYAN}y{YELLOW}O{BLUE}u{LIGHT_MAGENTA}'L{RED}l{YELLOW} f{LIGHT_CYAN}L{RED}o{LIGHT_CYAN}A{YELLOW}t{GREEN} {BLUE}t{LIGHT_MAGENTA}O{GREEN}o! 🎈🎈🎈        {NC}")
